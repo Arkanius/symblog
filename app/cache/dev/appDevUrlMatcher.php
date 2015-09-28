cc8a64e3d0f7908d5960608093f0c471e3ec2985
Gazotti <vtr.gomes@hotmail.com>
2015-09-27 22:40:09 -0300

    changes

--- app/cache/dev/appDevUrlMatcher.php
+++ app/cache/dev/appDevUrlMatcher.php
@@ -127,13 +127,93 @@ class appDevUrlMatcher extends Symfony\Bundle\FrameworkBundle\Routing\Redirectab
 
         }
 
-        // homepage
-        if (rtrim($pathinfo, '/') === '') {
-            if (substr($pathinfo, -1) !== '/') {
-                return $this->redirect($pathinfo.'/', 'homepage');
-            }
+        // study_symblog_default_index
+        if (0 === strpos($pathinfo, '/hello') && preg_match('#^/hello/(?P<name>[^/]++)$#s', $pathinfo, $matches)) {
+            return $this->mergeDefaults(array_replace($matches, array('_route' => 'study_symblog_default_index')), array (  '_controller' => 'study\\symblogBundle\\Controller\\DefaultController::indexAction',));
+        }
 
-            return array (  '_controller' => 'AppBundle\\Controller\\DefaultController::indexAction',  '_route' => 'homepage',);
+        if (0 === strpos($pathinfo, '/post')) {
+            // post
+            if (rtrim($pathinfo, '/') === '/post') {
+                if (!in_array($this->context->getMethod(), array('GET', 'HEAD'))) {
+                    $allow = array_merge($allow, array('GET', 'HEAD'));
+                    goto not_post;
+                }
+
+                if (substr($pathinfo, -1) !== '/') {
+                    return $this->redirect($pathinfo.'/', 'post');
+                }
+
+                return array (  '_controller' => 'study\\ModelBundle\\Controller\\PostController::indexAction',  '_route' => 'post',);
+            }
+            not_post:
+
+            // post_create
+            if ($pathinfo === '/post/') {
+                if ($this->context->getMethod() != 'POST') {
+                    $allow[] = 'POST';
+                    goto not_post_create;
+                }
+
+                return array (  '_controller' => 'study\\ModelBundle\\Controller\\PostController::createAction',  '_route' => 'post_create',);
+            }
+            not_post_create:
+
+            // post_new
+            if ($pathinfo === '/post/new') {
+                if (!in_array($this->context->getMethod(), array('GET', 'HEAD'))) {
+                    $allow = array_merge($allow, array('GET', 'HEAD'));
+                    goto not_post_new;
+                }
+
+                return array (  '_controller' => 'study\\ModelBundle\\Controller\\PostController::newAction',  '_route' => 'post_new',);
+            }
+            not_post_new:
+
+            // post_show
+            if (preg_match('#^/post/(?P<id>[^/]++)$#s', $pathinfo, $matches)) {
+                if (!in_array($this->context->getMethod(), array('GET', 'HEAD'))) {
+                    $allow = array_merge($allow, array('GET', 'HEAD'));
+                    goto not_post_show;
+                }
+
+                return $this->mergeDefaults(array_replace($matches, array('_route' => 'post_show')), array (  '_controller' => 'study\\ModelBundle\\Controller\\PostController::showAction',));
+            }
+            not_post_show:
+
+            // post_edit
+            if (preg_match('#^/post/(?P<id>[^/]++)/edit$#s', $pathinfo, $matches)) {
+                if (!in_array($this->context->getMethod(), array('GET', 'HEAD'))) {
+                    $allow = array_merge($allow, array('GET', 'HEAD'));
+                    goto not_post_edit;
+                }
+
+                return $this->mergeDefaults(array_replace($matches, array('_route' => 'post_edit')), array (  '_controller' => 'study\\ModelBundle\\Controller\\PostController::editAction',));
+            }
+            not_post_edit:
+
+            // post_update
+            if (preg_match('#^/post/(?P<id>[^/]++)$#s', $pathinfo, $matches)) {
+                if ($this->context->getMethod() != 'PUT') {
+                    $allow[] = 'PUT';
+                    goto not_post_update;
+                }
+
+                return $this->mergeDefaults(array_replace($matches, array('_route' => 'post_update')), array (  '_controller' => 'study\\ModelBundle\\Controller\\PostController::updateAction',));
+            }
+            not_post_update:
+
+            // post_delete
+            if (preg_match('#^/post/(?P<id>[^/]++)$#s', $pathinfo, $matches)) {
+                if ($this->context->getMethod() != 'DELETE') {
+                    $allow[] = 'DELETE';
+                    goto not_post_delete;
+                }
+
+                return $this->mergeDefaults(array_replace($matches, array('_route' => 'post_delete')), array (  '_controller' => 'study\\ModelBundle\\Controller\\PostController::deleteAction',));
+            }
+            not_post_delete:
+
         }
 
         throw 0 < count($allow) ? new MethodNotAllowedException(array_unique($allow)) : new ResourceNotFoundException();
